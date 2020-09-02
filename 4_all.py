d897a0fae03b7952908c0d7793b3c6c2f7bf20ac
kia-master <kia.iri.kia@gmail.com>
2020-09-02 22:32:00 +0300

    update file readme

--- 4_all.py
+++ 4_all.py
@@ -42,6 +42,7 @@ vlans = vlan1.intersection(vlan2)
 # Next-Hop              10.0.13.3
 # Last update           3d18h
 # Outbound Interface    FastEthernet0/0
+
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
 string = ospf_route.split()
 
